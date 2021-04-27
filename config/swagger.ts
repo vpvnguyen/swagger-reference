cbe494efde84b28b7ecd7fbfddca09ab1e02aea9
vpvnguyen <vpvnguyen@gmail.com>
2021-04-27 16:06:37 -0700

    create example swagger setup and serve config

---
+++ config/swagger.ts
@@ -0,0 +1,36 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/books",
+      },
+    ],
+  },
+  apis: ["./routes/books.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+const swaggerServe = swaggerUi.serve;
+const swaggerSetup = swaggerUi.setup(specs);
+
+export { swaggerServe, swaggerSetup };
