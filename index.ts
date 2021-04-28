8843f0e5bf0dce0184360e6b5b5ab81d7566a33d
vpvnguyen <vpvnguyen@gmail.com>
2021-04-28 12:44:07 -0700

    add swagger setup and routes; add books router

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { swaggerServe, swaggerSetup } from "./config/swagger";
+
+import booksRouter from "./api/books";
 
 const app = express();
 const PORT = 5000;
 
+app.use("/api-docs", swaggerServe, swaggerSetup);
+
+app.use("/books", booksRouter);
+
 app.get("/", (req, res) => {
-  res.json("Swagger Reerence");
+  res.json("Swagger Reference");
 });
 
 app.listen(PORT, () =>
